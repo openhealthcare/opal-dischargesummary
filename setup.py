c2c27e9eca099be222e85090e45bc93af8c5860d
David Miller <david@deadpansincerity.com>
2016-06-16 15:31:28 +0100

    Update setup.py version to 0.3.0"

--- setup.py
+++ setup.py
@@ -8,10 +8,10 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='opal-dischargesummary',
-    version='0.1',
+    version='0.3.0',
     packages=['dischargesummary'],
     include_package_data=True,
-    license='LICENSE',  
+    license='LICENSE',
     description='The dischargesummary OPAL Plugin',
     long_description=README,
     url='http://opal.openhealthcare.org.uk/',
