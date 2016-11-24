f8b9074af32a6e71c3b63389738b9b18cf56ab89
fredkingham <fredkingham@gmail.com>
2016-11-24 22:05:23 +0000

    updated to use the new karma defaults

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -8,7 +8,7 @@ module.exports = function(config){
     opalPath = '../../opal';
   }
   var karmaDefaults = require(opalPath + '/config/karma_defaults.js');
-  var karmaDir = __dirname;
+  var baseDir = __dirname + '/..';
   var coverageFiles = [
     __dirname +  '/../dischargesummary/static/js/dischargesummary/controllers/*.js',
   ];
@@ -17,6 +17,6 @@ module.exports = function(config){
     __dirname + '/../dischargesummary/static/js/test/*.js',
   ];
 
-  var defaultConfig = karmaDefaults(karmaDir, coverageFiles, includedFiles);
+  var defaultConfig = karmaDefaults(includedFiles, baseDir, coverageFiles);
   config.set(defaultConfig);
 };
