9ed0d10d826fbd8de07f45d74f89743f8cea0645
David Miller <david@deadpansincerity.com>
2018-01-02 15:29:58 +0000

    Bump version number

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='opal-dischargesummary',
-    version='0.4.0',
+    version='0.5.0',
     packages=['dischargesummary'],
     include_package_data=True,
     license='LICENSE',
